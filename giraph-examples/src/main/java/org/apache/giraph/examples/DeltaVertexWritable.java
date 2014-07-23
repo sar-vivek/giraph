9160384994637ede421eda82343f1a363a97f6c0
Vivek Sardeshmukh <>
2014-07-23 12:17:12 -0500

    Debugging DeltaVertexWritable!

--- giraph-examples/src/main/java/org/apache/giraph/examples/DeltaVertexWritable.java
+++ giraph-examples/src/main/java/org/apache/giraph/examples/DeltaVertexWritable.java
@@ -1,30 +1,16 @@
-/*
- * Copyright 2014 The Apache Software Foundation.
+/**
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * @author Vivek B Sardeshmukh
  */
+
 package org.apache.giraph.examples;
 
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
 import org.apache.hadoop.io.Writable;
- 
-/**
- *
- * @author Vivek B Sardeshmukh
- */
-class DeltaVertexWritable implements Writable {
+
+public class DeltaVertexWritable implements Writable {
     /**
      * Minimum distance to source node
      */
@@ -89,6 +75,7 @@ class DeltaVertexWritable implements Writable {
     } 
     @Override
     public String toString() {
-        return "(dist = " + dist + ", bucket = " + bucket + ", flag = " + doneLight + ")";
+        return "(dist = " + dist + ", bucket = " + bucket + ", flag = " + 
+                doneLight + ")";
     }
 }
