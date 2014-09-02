5304014300d7fbbb52a89be110d0fa05385d169d
vivek-sardeshmukh@uiowa.edu <vivek-sardeshmukh@uiowa.edu>
2014-09-02 14:12:04 -0500

    added new input formats to support SNAP

---
+++ giraph-examples/src/main/java/org/apache/giraph/examples/LongDoubleNullVertexInputFormat.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.giraph.examples;
+
+import com.google.common.collect.ImmutableList;
+import java.io.IOException;
+import java.util.regex.Pattern;
+import org.apache.giraph.edge.Edge;
+import org.apache.giraph.io.formats.TextVertexInputFormat;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+/**
+ * Simple text-based {@link org.apache.giraph.io.VertexInputFormat} for
+ * unweighted graphs with int ids.
+ *
+ * Each line consists of: vertex_id vertex_value 
+ */
+public class LongDoubleNullVertexInputFormat
+    extends
+    TextVertexInputFormat<LongWritable, DoubleWritable, NullWritable> {
+  /** Separator of the vertex and neighbors */
+  private static final Pattern SEPARATOR = Pattern.compile("[\t ]");
+
+  @Override
+  public TextVertexReader createVertexReader(InputSplit split,
+      TaskAttemptContext context)
+    throws IOException {
+    return new LongDoubleNullVertexReader();
+  }
+
+  /**
+   * Vertex reader associated with
+   * {@link org.apache.giraph.io.formats.IntIntNullTextVertexInputFormat}.
+   */
+  public class LongDoubleNullVertexReader extends
+    TextVertexReaderFromEachLineProcessed<String[]> {
+    /** Cached vertex id for the current line */
+    private LongWritable id;
+    /** Cached vertex value for the current line */
+    private DoubleWritable value;
+
+    @Override
+    protected String[] preprocessLine(Text line) throws IOException {
+      String[] tokens = SEPARATOR.split(line.toString());
+      id = new LongWritable(Long.parseLong(tokens[0]));
+      value = new DoubleWritable(Double.parseDouble(tokens[1]));
+      return tokens;
+    }
+
+    @Override
+    protected LongWritable getId(String[] tokens) throws IOException {
+      return id;
+    }
+
+    @Override
+    protected DoubleWritable getValue(String[] tokens) throws IOException {
+      return value;
+    }
+		
+    @Override
+    protected Iterable<Edge<LongWritable, NullWritable>> getEdges(String[] tokens)
+      throws IOException {
+      return ImmutableList.of();
+    }
+  }
+}
---
+++ giraph-examples/src/main/java/org/apache/giraph/examples/LongFloatTextEdgeInputFormat.java
@@ -0,0 +1,79 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.giraph.examples;
+
+import java.io.IOException;
+import java.util.regex.Pattern;
+import org.apache.giraph.examples.utils.LongPair;
+import org.apache.giraph.io.EdgeReader;
+import org.apache.giraph.io.formats.TextEdgeInputFormat;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.InputSplit;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+/**
+ * Simple text-based {@link org.apache.giraph.io.EdgeInputFormat} for
+ * unweighted graphs with long ids.
+ *
+ * Each line consists of: source_vertex, target_vertex
+ */
+public class LongFloatTextEdgeInputFormat extends
+    TextEdgeInputFormat<LongWritable, FloatWritable> {
+  /** Splitter for endpoints */
+  private static final Pattern SEPARATOR = Pattern.compile("[\t ]");
+
+  @Override
+  public EdgeReader<LongWritable, FloatWritable> createEdgeReader(
+      InputSplit split, TaskAttemptContext context) throws IOException {
+    return new LongFloatTextEdgeReader();
+  }
+
+  /**
+   * {@link org.apache.giraph.io.EdgeReader} associated with
+   * {@link IntNullTextEdgeInputFormat}.
+   */
+  public class LongFloatTextEdgeReader extends
+      TextEdgeReaderFromEachLineProcessed<LongPair> {
+    @Override
+    protected LongPair preprocessLine(Text line) throws IOException {
+      String[] tokens = SEPARATOR.split(line.toString());
+      return new LongPair(Long.valueOf(tokens[0]),
+          Long.valueOf(tokens[1]));
+    }
+
+    @Override
+    protected LongWritable getSourceVertexId(LongPair endpoints)
+      throws IOException {
+      return new LongWritable(endpoints.getFirst());
+    }
+
+    @Override
+    protected LongWritable getTargetVertexId(LongPair endpoints)
+      throws IOException {
+      return new LongWritable(endpoints.getSecond());
+    }
+
+    @Override
+    protected FloatWritable getValue(LongPair endpoints) throws IOException {
+      return new FloatWritable(1f);
+    }
+  }
+}
